ba612f217b47e2169cc32cbf6f4d350ad52036a8
Justin Steward <justin@justinsteward.com>
2013-05-22 07:31:52 +1000

    fix typo, start making way for configuring options

--- manifests/init.pp
+++ manifests/init.pp
@@ -6,10 +6,17 @@ class bind (
 
     include concat::setup
 
-    concat { "/etc/bind/named.local":
+    concat { "/etc/bind/named.conf.local":
         owner => 'root',
         group => 'root',
         mode  => '0644',
         notify => Service['bind']
     }
+    
+    file { "/etc/bind/named.conf.options":
+        ensure  => 'present',
+        owner   => 'root',
+        group   => 'root',
+        mode    => '0644',
+        content => template('bind/named.conf.options.erb'),
 }
