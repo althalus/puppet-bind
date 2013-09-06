b8b55fb0f4458415aa47ebde59ff6ba09e2ba398
Justin Steward <justin@justinsteward.com>
2013-09-06 14:55:13 +1000

    Small changes to defaults

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,11 +1,12 @@
 class bind (
     $package_version = 'installed',
-    $recurion        = 'no',
-    $allow_recursion = [],
+    $recursion       = 'no',
+    $allow_recursion = undef,
     $listen_on       = [ 'any' ],
-    $listen_on_v6   = [ 'any' ],
-    $allow_transfer  = [],
-    $allow_notify    = [],
+    $listen_on_v6    = [ 'any' ],
+    $allow_transfer  = undef,
+    $notify          = "yes",
+    $allow_notify    = undef,
 ) inherits bind::params {
     include bind::install
     include bind::service
